3f63670d77305f06470a4ee3e71ea68e7e039cd2
Patrick Doane <patrickdoane@gmail.com>
2024-01-26 16:47:35 -0600

    Extract page logic from App.js to Page.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+import Page from './components/Page.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <nav className="App-nav">
-        <p>nav</p>
-      </nav>
-      <header className="App-header">
-          <menu className="App-menu">
-            <button className="menu-button-navigation"></button>
-            <p>tarkov tracker</p>
-            <button className="menu-button-options"></button>
-          </menu>
-      </header>
-      <main className="App-main">
-        <p>main</p>
-      </main>
-      <footer className="App-footer">
-        <p>footer</p>
-      </footer>
+        <Page /> 
     </div>
   );
 }
---
+++ src/components/Page.jsx
@@ -0,0 +1,24 @@
+const Page = () => {
+    return (
+        <div className="App-page">
+            <nav className="App-nav">
+            <p>nav</p>
+            </nav>
+            <header className="App-header">
+                <menu className="App-menu">
+                    <button className="menu-button-navigation"></button>
+                    <p>tarkov tracker</p>
+                    <button className="menu-button-options"></button>
+                </menu>
+            </header>
+            <main className="App-main">
+                <p>main</p>
+            </main>
+            <footer className="App-footer">
+                <p>footer</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Page;
